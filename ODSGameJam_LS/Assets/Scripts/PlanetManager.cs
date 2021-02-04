cf015d98e4b43b5db38e8196cad7298a030a4429
Joan Marín <36265613+X0KA@users.noreply.github.com>
2021-02-04 12:06:36 +0100

    Update PlanetManager.cs

--- ODSGameJam_LS/Assets/Scripts/PlanetManager.cs
+++ ODSGameJam_LS/Assets/Scripts/PlanetManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
-    public float rotationSpeed=10.0f;
-
+    public float rotationSpeed=5.0f;
+    public float deaceleration = 0.4f;
     private bool rotatingPlanet = false;
+
+    private Vector2 currentVelocity = new Vector2(0,0);
+
+    public Camera camera;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +25,54 @@ public class PlanetManager : MonoBehaviour
             rotatingPlanet = true;
         if (Input.GetMouseButtonUp(0)) 
             rotatingPlanet = false;
-        
+
         if (rotatingPlanet)
-            RotatePlanet();
+        {
+            currentVelocity.x = Input.GetAxis("Mouse X");
+            currentVelocity.y = Input.GetAxis("Mouse Y");
+        }
+        RotatePlanet();
+        
+        if (!rotatingPlanet)
+            UpdateSpeed();
     }
 
     private void RotatePlanet()
     {
-        gameObject.transform.Rotate(new Vector3(0, 1, 0), -Input.GetAxis("Mouse X")* rotationSpeed, Space.World);
-        gameObject.transform.Rotate(new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * rotationSpeed, Space.World);
+        camera.gameObject.transform.RotateAround(gameObject.transform.position,camera.transform.up, currentVelocity.x * rotationSpeed);
+        camera.gameObject.transform.RotateAround(gameObject.transform.position, camera.transform.right, -currentVelocity.y * rotationSpeed);
+    }
 
+    //Reduce speed
+    private void UpdateSpeed()
+    {
+        //X axis
+        if (currentVelocity.x > 0.0001f)
+        {
+            currentVelocity.x = currentVelocity.x- deaceleration*Time.deltaTime;
+            Debug.Log(currentVelocity.x);
+            if (currentVelocity.x < 0.0001f)
+                currentVelocity.x = 0.0000f;
+        }
+        else if (currentVelocity.x < 0.0001f)
+        {
+            currentVelocity.x = currentVelocity.x + deaceleration * Time.deltaTime;
+            if (currentVelocity.x > 0.0001f)
+                currentVelocity.x = 0.0001f;
+        }
+
+        //Y axis
+        if (currentVelocity.y > 0.0001f)
+        {
+            currentVelocity.y = currentVelocity.y - deaceleration * Time.deltaTime;
+            if (currentVelocity.y < 0.0001f)
+                currentVelocity.y = 0.0001f;
+        }
+        else if (currentVelocity.y < 0.0001f)
+        {
+            currentVelocity.y = currentVelocity.y + deaceleration * Time.deltaTime;
+            if (currentVelocity.y > 0.0001f)
+                currentVelocity.y = 0.0001f;
+        }
     }
 }
