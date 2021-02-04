33fc13f407bc026535a02e8af4ca0c32708f0308
Joan Marín <36265613+X0KA@users.noreply.github.com>
2021-02-04 10:51:25 +0100

    Rotation script

---
+++ ODSGameJam_LS/Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+    public float rotationSpeed=10.0f;
+
+    private bool rotatingPlanet = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            rotatingPlanet = true;
+        if (Input.GetMouseButtonUp(0)) 
+            rotatingPlanet = false;
+        
+        if (rotatingPlanet)
+            RotatePlanet();
+    }
+
+    private void RotatePlanet()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 1, 0), -Input.GetAxis("Mouse X")* rotationSpeed, Space.World);
+        gameObject.transform.Rotate(new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * rotationSpeed, Space.World);
+
+    }
+}
