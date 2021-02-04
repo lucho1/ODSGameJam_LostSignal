fa642b2c5b1683765099455c39e0613ac258f025
Sergi PR <t3m1X@users.noreply.github.com>
2021-02-04 15:36:47 +0100

    Added Factory script

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/Factory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+
+    public bool EcoFactory = false;
+
+    private float m_contaminationAmount = 0.0f;
+    private float m_nextResourceTime = 0.0f;
+    private float m_nextContaminationTime = 0.0f;
+
+    private PlanetScript myPlanet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlanet = gameObject.GetComponent<PlanetScript>();
+        m_nextResourceTime = Time.time + GameOptions.ResourceRefreshRate;
+        m_nextContaminationTime = Time.time + GameOptions.PollutionRefreshRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= m_nextContaminationTime) {
+            m_nextContaminationTime = Time.time + GameOptions.PollutionRefreshRate;
+            myPlanet.currentHealth -= GameOptions.StandardFactoryContamination;
+        }
+
+        if (Time.time >= m_nextResourceTime) {
+            m_nextResourceTime = Time.time + GameOptions.PollutionRefreshRate;
+            // Add resources here
+        }
+    }
+}
