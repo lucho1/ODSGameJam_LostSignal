9a837c2d4837f9cc9b23b0a31a0995ffae6eead0
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2021-02-04 11:36:03 +0100

    ResourcesManager
    first version

--- ODSGameJam_LS/Assets/01_GAME/Scripts/ResourcesManager.cs
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/ResourcesManager.cs
@@ -10,19 +10,47 @@ This script is suposed to manage global data: Resources for example
 */
 
 
-public class ResourcesManager : MonoBehaviour
+public class ResourcesManager : Singleton<ResourcesManager>
 {
 
     // Start is called before the first frame update
     int resources;
+    int ammountOfPlanets;
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateResources", 1.0f, 0.5f);
+        resources = GameManager.CurrentResources;
+        if (GameManager.PlanetList.Capacity > 0)
+        {
+            ammountOfPlanets = GameManager.PlanetList.Count;
+        }  
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void UpdateResources() //called every x frames
+    {
+        GameManager.CurrentResources = resources;
+    }
+
+
+    //USER FUNCTIONS
+
+    static void AddResources(int ammount) //called from other scripts
+    {
+        Instance.resources = Instance.resources + ammount;
+    }
+    static void SubstractResources(int ammount) //called from other scripts
+    {
+        Instance.resources = Instance.resources - ammount;
+    }
+
+
+
 }
