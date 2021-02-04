52c1c4313d70bbc17ec86d59f584628320a3589f
lucho1 <luchosuaya@hotmail.com>
2021-02-04 16:24:30 +0100

    Added HUD for Planets

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/Planets/PlanetHUDScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetHUDScript : MonoBehaviour
+{
+    private PlanetManager m_PlanetManager;
+
+    [SerializeField]
+    private Text ColonizedPlanetsText;
+
+    [SerializeField]
+    private GameObject NextPlanetBtn, PrevPlanetBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlanetManager = GetComponentInParent<PlanetManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.PlanetList.Count > 1)
+        {
+            NextPlanetBtn.SetActive(true);
+            PrevPlanetBtn.SetActive(true);
+        }
+    }
+
+    public void NextPlanetButton()
+    {
+        //m_PlanetManager.SwitchPlanet();
+    }
+
+    public void PrevPlanetButton()
+    {
+        //m_PlanetManager.SwitchPlanet(false);
+    }
+
+    public void ColonizePlanetButton()
+    {
+        ColonizedPlanetsText.text = "Colonized Planets: " + GameManager.PlanetList.Count.ToString();
+
+    }
+}
