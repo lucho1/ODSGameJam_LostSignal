92277843049f8e0dcfc8d6991ee41a9a6726c918
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2021-02-04 11:05:42 +0100

    added script resourcesmanager

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/ResourcesManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+
+This script is suposed to manage global data: Resources for example 
+ 
+*/
+
+
+public class ResourcesManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    int resources;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
