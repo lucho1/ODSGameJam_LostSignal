595975ed6bdc0a6314cd637c6960695ddbed665b
Joan Marín <36265613+X0KA@users.noreply.github.com>
2021-02-04 12:51:35 +0100

    Update PlanetManager.cs

--- ODSGameJam_LS/Assets/Scripts/PlanetManager.cs
+++ ODSGameJam_LS/Assets/Scripts/PlanetManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
+    [Space(10)] [Header("Planet atributes")]
+    
+    public int planetHealthRecoveryRate = 5;    //Health recovered per second
+    public int currentHealth =100;              //Planet's health from 0 to 100
+    
+    //Timers
+    public float resourceUpdatePeriod = 1.0f;   //Every x seconds the resources produced by this planet will be updated on the resource manager
+    private float lastResourceUpdateTime = 0.0f;
+    private int resourcesIncome = 0;
+
+    [Space(10)] [Header("Rotation atribute")]
     public float rotationSpeed=5.0f;
     public float deaceleration = 0.4f;
     private bool rotatingPlanet = false;
-
     private Vector2 currentVelocity = new Vector2(0,0);
 
+    [Space(10)]
     public Camera camera;
+
+    private bool cameraAttached = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +34,30 @@ public class PlanetManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            rotatingPlanet = true;
-        if (Input.GetMouseButtonUp(0)) 
-            rotatingPlanet = false;
-
-        if (rotatingPlanet)
+        if (Time.time - lastResourceUpdateTime > resourceUpdatePeriod)
         {
-            currentVelocity.x = Input.GetAxis("Mouse X");
-            currentVelocity.y = Input.GetAxis("Mouse Y");
+            lastResourceUpdateTime = Time.time;
+            //Update resources on the thing
         }
-        RotatePlanet();
-        
-        if (!rotatingPlanet)
-            UpdateSpeed();
+
+        //update camera rotation if the planet is being looked at
+        if (cameraAttached)
+        {
+            if (Input.GetMouseButtonDown(0))
+                rotatingPlanet = true;
+            if (Input.GetMouseButtonUp(0))
+                rotatingPlanet = false;
+
+            if (rotatingPlanet)
+            {
+                currentVelocity.x = Input.GetAxis("Mouse X");
+                currentVelocity.y = Input.GetAxis("Mouse Y");
+            }
+            RotatePlanet();
+
+            if (!rotatingPlanet)
+                UpdateSpeed();
+        }
     }
 
     private void RotatePlanet()
@@ -74,5 +97,22 @@ public class PlanetManager : MonoBehaviour
             if (currentVelocity.y > 0.0001f)
                 currentVelocity.y = 0.0001f;
         }
+    }
+
+    public void UpdateIncome(int aditionalIncome)
+    {
+        resourcesIncome = resourcesIncome + aditionalIncome;
+        if (resourcesIncome < 0)
+            resourcesIncome = 0;
+    }
+
+    public void AttachCamera()
+    {
+        cameraAttached = true;
+    }
+
+    public void DetachCamera()
+    {
+        cameraAttached = false;
     }
 }
