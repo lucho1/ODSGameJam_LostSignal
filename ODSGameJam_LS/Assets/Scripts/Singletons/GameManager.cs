0a7957f32515ae9bf1bd1ee59ced87834838ddf4
Sergi PR <t3m1X@users.noreply.github.com>
2021-02-04 11:00:18 +0100

    Added GameManager for temporal scene values

---
+++ ODSGameJam_LS/Assets/Scripts/Singletons/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+public class GameManager : Singleton<GameManager>
+{
+    protected GameManager() {}
+    
+    private List<PlanetScript> m_planetList;
+    public static List<PlanetScript> PlanetList {
+        get {
+            return Instance.m_planetList;
+        }
+        set {
+            Instance.m_planetList = value;
+        }
+    }
+
+    private int m_currentResources;
+    public static int CurrentResources {
+        get {
+            return Instance.m_currentResources;
+        }
+        set {
+            Instance.m_currentResources = value;
+        }
+    }
+}
