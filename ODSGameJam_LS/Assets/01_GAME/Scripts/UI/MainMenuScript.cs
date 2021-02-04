18293d91365f4b248413ff0bd18f02da249ba941
lucho1 <luchosuaya@hotmail.com>
2021-02-04 14:45:51 +0100

    Main Menu Screen

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MenuObject;
+
+    [SerializeField]
+    private GameObject CreditsObject;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void CreditsButton()
+    {
+        MenuObject.SetActive(false);
+        CreditsObject.SetActive(true);
+    }
+
+    public void CreditsReturnButton()
+    {
+        CreditsObject.SetActive(false);
+        MenuObject.SetActive(true);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
