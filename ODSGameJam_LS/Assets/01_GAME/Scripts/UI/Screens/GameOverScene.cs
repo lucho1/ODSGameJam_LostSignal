5f613a392122859be31a935805a2639982127f63
lucho1 <luchosuaya@hotmail.com>
2021-02-05 11:38:12 +0100

    Game over scene implementing game results

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/UI/Screens/GameOverScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScene : MonoBehaviour
+{
+    [SerializeField]
+    private Text TitleText, VictoryText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameResults.IsVictory)
+        {
+            TitleText.text = "Victory!";
+            VictoryText.text = "You managed to save the planet!\nYou needed to colonize " + GameResults.TotalPlanets +
+                                " planets to accomplish it, and you killed " + GameResults.DeadPlanets + " in your way."
+                                    + "\n\nYou also got the whole needed resources to save The Earth and gathered additional "
+                                        + GameResults.FinalResources + " resources.";
+        }
+        else
+        {
+            TitleText.text = "Game Over";
+            VictoryText.text = "Oh no! Our planet was destroyed by polution\nYou needed to colonize " + GameResults.TotalPlanets +
+                                " planets and kill " + GameResults.DeadPlanets + " other planets in your way to save The Earth and yet you didn't accomplish it"
+                                    + "\n\nYou finally stored " + GameResults.FinalResources + " resources.";
+        }
+    }
+}
