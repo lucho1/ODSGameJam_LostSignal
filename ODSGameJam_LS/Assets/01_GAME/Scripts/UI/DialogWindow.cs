534812f74c855394d90ecb947979b35917ea84df
Joan Marín <36265613+X0KA@users.noreply.github.com>
2021-02-05 14:54:53 +0100

    Update DialogWindow.cs

--- ODSGameJam_LS/Assets/01_GAME/Scripts/UI/DialogWindow.cs
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/UI/DialogWindow.cs
@@ -15,28 +15,30 @@ public class DialogWindow : MonoBehaviour
     public GameObject closeButton;
 
 
-
-    private string GameStartText = "We need some energy! Start builing factories.";
-    private string GameStartText2 = "If you run out of space in the planet, try colonizing other planets to build there. Make Valentina Tereshkova proud!";
-    private string FirstfactoryText = "Wow! Look at those factories! I hope those chimneys have some filters though.Which by the way, were invented by Mary Walton in 1870. That's it, some free knowledge for you.";
-    private string ManyFactoriesText = "Be careful with those factories, they polute a lot. Wangari Maathai wouldn't like it. She won a nobel prize, you probably don't know her...";
-    private string AnotherOneText = "And aonther one!";
-    private string FirstCleanFactorytext = "Hey, that factory does not polute at all! Nice!";
-    private string CleanFactoryText = "This is the legacy of Hertha Marks!";
-    private string CleanFactoryText2 = "Give me that green energy baby!";
-    private string FactoryDestroyed = "Well, there it goes a lot of energy income, but reducing the polution is more important. Greta Thumberg would be proud for sure.";
-    private string FactoryDestroyed2 = "Down it goes!"; 
-    private string FactoryDestroyed3 = "We might run out of energy but I have to admit, it is really fun to see things go down.";
-
+    [HideInInspector] public string GameStartText = "We need some energy! Start builing factories.";
+    [HideInInspector] public string GameStartText2 = "If you run out of space in the planet, try colonizing other planets to build there. Make Valentina Tereshkova proud!";
+    [HideInInspector]public string FirstfactoryText = "Wow! Look at those factories! I hope those chimneys have some filters though.Which by the way, were invented by Mary Walton in 1870. That's it, some free knowledge for you.";
+    [HideInInspector]public string ManyFactoriesText = "Be careful with those factories, they polute a lot. Wangari Maathai wouldn't like it. She won a nobel prize, you probably don't know her...";
+    [HideInInspector]public string AnotherOneText = "And aonther one!";
+    [HideInInspector]public string FirstCleanFactorytext = "Hey, that factory does not polute at all! Nice!";
+    [HideInInspector]public string CleanFactoryText = "This is the legacy of Hertha Marks!";
+    [HideInInspector]public string CleanFactoryText2 = "Give me that green energy baby!";
+    [HideInInspector]public string FactoryDestroyed = "Well, there it goes a lot of energy income, but reducing the polution is more important. Greta Thumberg would be proud for sure.";
+    [HideInInspector]public string FactoryDestroyed2 = "Down it goes!";
+    [HideInInspector] public string FactoryDestroyed3 = "We might run out of energy but I have to admit, it is really fun to see things go down.";
+    
     // Start is called before the first frame update
     void Start()
     {
         dialog.text = GameStartText2;
+        dialogTimer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time - dialogTimer > disappearTime)
+            DeactivateDialog();
     }
 
     void ActivateDialog()
@@ -46,5 +48,13 @@ public class DialogWindow : MonoBehaviour
         imageGO.SetActive(true);
         closeButton.SetActive(true);
         dialogTimer = Time.time;
+    }
+
+    void DeactivateDialog()
+    {
+        nameGO.SetActive(false);
+        dialogGO.SetActive(false);
+        imageGO.SetActive(false);
+        closeButton.SetActive(false);
     }
 }
