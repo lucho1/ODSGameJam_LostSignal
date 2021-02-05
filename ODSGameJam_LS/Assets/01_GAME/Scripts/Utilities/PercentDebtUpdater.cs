8753e2d0d148515f80f8f68046b8dbc08274576e
Sergi PR <t3m1X@users.noreply.github.com>
2021-02-05 18:27:58 +0100

    Added percent sum indicator

---
+++ ODSGameJam_LS/Assets/01_GAME/Scripts/Utilities/PercentDebtUpdater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentDebtUpdater : MonoBehaviour
+{
+    Text m_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_text = GetComponent<Text>();
+        int percent = Mathf.RoundToInt(GameOptions.IncreasedDebt * 100);
+        m_text.text ="+" + percent.ToString() + "%";
+    }
+}
